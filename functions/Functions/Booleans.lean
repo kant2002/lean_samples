19add3012e967fd7c8505ff250390a4416c3516c
Andrii Kurdiumov <kant2002@gmail.com>
2024-11-30 10:28:53 +0500

    format for presentation

--- functions/Functions/Booleans.lean
+++ functions/Functions/Booleans.lean
@@ -1,9 +1,13 @@
 namespace Booleans
--- Booleans values are 'true' and 'false' as in regular languages
+-- Booleans values are
+-- 'true' and 'false' as in regular languages
 def boolean1 := true
 def boolean2 := false
 
--- Operators on booleans are 'not', '&&' and '||'.
-def boolean3 := not boolean1 && (boolean2 || false)
+-- Operators on booleans are
+-- 'not', '&&' and '||'.
+def boolean3 :=
+  not boolean1
+  && (boolean2 || false)
 
 end Booleans
--- functions/Functions/StringManipulation.lean
+++ functions/Functions/StringManipulation.lean
@@ -3,18 +3,28 @@ namespace StringManipulation
 def string1 : String := "Hello"
 def string2  := "world"
 
--- Strings can also use r## to create a raw string literal.
--- This will ignore escape characters such as '\', '\n', '\t', etc.
+-- Strings can also use r## to create a
+-- raw string literal.
+-- This will ignore escape characters
+-- such as '\\', '\n', '\t', etc.
 def string3 := r"C:\Program Files\"
 
--- String literals can also use r## to escape # inside raw stings.
-def string4 := r##"No # will escape prison of raw literals!"##
+-- String literals can also use r## to
+-- escape # inside raw stings.
+def string4 :=
+  r##"No # will escape prison of raw literals!"##
 
--- String concatenation is normally done with the '++' operator.
-def helloWorld := string1 ++ " " ++ string2
+-- String concatenation is
+-- usually done with the '++' operator.
+def helloWorld :=
+  string1 ++ " " ++ string2
 
--- Substrings use the indexer notation.  This line extracts the first 7 characters as a substring.
--- Note that like many languages, Strings are zero-indexed in F#.
+-- Substrings use the indexer notation.
+-- This line extracts the first 7 characters as a substring.
+-- Note that like many languages,
+-- Strings are zero-indexed in Lean.
 def substring := helloWorld.extract ⟨0⟩ ⟨6⟩
+
+def removedWhitespaces := helloWorld.trim
 
 end StringManipulation
