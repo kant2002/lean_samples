e1f31e24255ab928e9f12dc841e6d19e2a7ddd7d
Andrii Kurdiumov <kant2002@gmail.com>
2024-11-30 10:26:08 +0500

    Add couple of operators for lists

--- datatypes/Datatypes/Lists.lean
+++ datatypes/Datatypes/Lists.lean
@@ -1,8 +1,10 @@
 namespace Lists
-    -- Lists are defined using [ ... ].  You should specify type for empty list.
+    -- Lists are defined using [ ... ].  You should specify
+    -- type for empty list.
     def list1 : List Nat := [ ]
 
-    -- This is a list with 3 elements.  ',' is used to separate elements on the same line.
+    -- This is a list with 3 elements.  ',' is used to separate
+    -- elements on the same line.
     def list2 := [ 1, 2, 3 ]
 
     -- You can also separate elements by placing them on their own lines.
@@ -12,11 +14,24 @@ namespace Lists
         3
     ]
 
+    -- You can combine lists using ++ operator
+    def list4 := list3 ++ list3
+
+    -- Accessing element by 0-based index.
+    def element := list4[5]
+
+    -- Lean automatically check that indexing
+    -- does not go out of range.
+    -- Try uncomment line below.
+    --#eval list4[6]
+
     -- This is a list of integers from 1 to 1000
     def numberList := List.range' 1 1000
 
-    -- Lists can be transformed using 'List.map' and other functional programming combinators.
-    -- This definition produces a new list by squaring the numbers in numberList, using the pipeline
+    -- Lists can be transformed using 'List.map' and other
+    -- functional programming combinators.
+    -- This definition produces a new list by squaring the
+    -- numbers in numberList, using the pipeline
     -- operator to pass an argument to List.map.
     def squares :=
         numberList
